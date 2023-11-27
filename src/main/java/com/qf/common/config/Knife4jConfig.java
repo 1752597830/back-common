d3b813417bc3dea5449aed0a691838b71abc8328
清风 <1752597830@qq.com>
2023-11-27 23:38:23 +0800

    解决冲突

--- src/main/java/com/qf/common/config/Knife4jConfig.java
+++ src/main/java/com/qf/common/config/Knife4jConfig.java
@@ -1,25 +1,18 @@
 package com.qf.common.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author : sin
- * @date : 2023/11/27 23:08
+ * @date : 2023/11/27 23:37
  * @Description :
  */
-@Configuration
-@EnableSwagger2
-@Profile({"dev", "test"})
 public class Knife4jConfig {
-
     @Bean
     public Docket defaultApi2() {
         return new Docket(DocumentationType.SWAGGER_2)
